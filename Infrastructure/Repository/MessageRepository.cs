43cb509894384e431504dc259d1a5f4d151590a9
htloc <lochatan19@gmail.com>
2021-06-29 16:48:08 +0700

    Repository

---
+++ Infrastructure/Repository/MessageRepository.cs
@@ -0,0 +1,52 @@
+﻿using Domain.Models;
+using Domain.Repository;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infrastructure.Repository
+{
+    public class MessageRepository : IMessageRepository
+    {
+        private MessageDBContext context;
+        
+        public MessageRepository()
+        {
+            context = new MessageDBContext();
+        }
+
+        public void Delete(long id)
+        {
+            var std = context.MessageTable.Find(id);
+            context.Remove(std);
+        }
+
+        public List<MessageModel> GetAll()
+        {
+            return context.MessageTable.ToList();
+        }
+
+        public MessageModel GetById(long id)
+        {
+            return context.MessageTable.Find(id);
+        }
+
+        public void Insert(MessageModel model)
+        {
+            context.MessageTable.Add(model);
+        }
+
+        public void SaveChange()
+        {
+            context.SaveChanges();
+        }
+
+        public void Update(MessageModel model)
+        {
+            context.MessageTable.Find(model.Id);
+            context.MessageTable.Update(model);
+        }
+    }
+}
