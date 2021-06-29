b59c0c1bfe1eb8870d38a76d1ce225acc0326204
htloc <lochatan19@gmail.com>
2021-06-29 16:40:22 +0700

    model

---
+++ Domain/Models/MessageModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class MessageModel
+    {
+        public long Id { get; set; }
+        public string Content { get; set; }
+        public string Title { get; set; }
+        public long SenderId { get; set; }
+        public long RecipientId { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
