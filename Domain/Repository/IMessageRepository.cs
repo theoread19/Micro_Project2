3d755922b516796b27c4bc0cc831cb2399bd5656
htloc <lochatan19@gmail.com>
2021-06-29 16:47:19 +0700

    repository interface

---
+++ Domain/Repository/IMessageRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Repository
+{
+    public interface IMessageRepository
+    {
+        public List<MessageModel> GetAll();
+        public MessageModel GetById(long id);
+        public void Insert(MessageModel model);
+        public void Update(MessageModel model);
+        public void Delete(long id);
+        public void SaveChange();
+    }
+}
