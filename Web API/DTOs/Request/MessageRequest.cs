4fbad1c129ae113e282771fbafd9efe12d5cf6fd
htloc <lochatan19@gmail.com>
2021-06-29 16:38:47 +0700

    object transfer bwt service & repo

---
+++ Web API/DTOs/Request/MessageRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_API.DTOs.Request
+{
+    public class MessageRequest
+    {
+        public long Id { get; set; }
+        public string Content { get; set; }
+        public string Title { get; set; }
+        public long SenderId { get; set; }
+        public long RecipientId { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
