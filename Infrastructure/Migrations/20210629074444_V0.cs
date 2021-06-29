7d8776132299c03458c18b52f01f715d97564e58
htloc <lochatan19@gmail.com>
2021-06-29 16:46:52 +0700

    migration

---
+++ Infrastructure/Migrations/20210629074444_V0.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class V0 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user-table",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    content = table.Column<string>(type: "text", nullable: false),
+                    title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    SenderId = table.Column<long>(type: "bigint", nullable: false),
+                    RecipientId = table.Column<long>(type: "bigint", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "DateTime", nullable: false, defaultValue: new DateTime(2021, 6, 29, 14, 44, 44, 335, DateTimeKind.Local).AddTicks(8361))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user-table", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user-table");
+        }
+    }
+}
---
+++ Infrastructure/Migrations/20210629074607_V1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class V1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_user-table",
+                table: "user-table");
+
+            migrationBuilder.RenameTable(
+                name: "user-table",
+                newName: "message-table");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateDate",
+                table: "message-table",
+                type: "DateTime",
+                nullable: false,
+                defaultValue: new DateTime(2021, 6, 29, 14, 46, 7, 366, DateTimeKind.Local).AddTicks(9341),
+                oldClrType: typeof(DateTime),
+                oldType: "DateTime",
+                oldDefaultValue: new DateTime(2021, 6, 29, 14, 44, 44, 335, DateTimeKind.Local).AddTicks(8361));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_message-table",
+                table: "message-table",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_message-table",
+                table: "message-table");
+
+            migrationBuilder.RenameTable(
+                name: "message-table",
+                newName: "user-table");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateDate",
+                table: "user-table",
+                type: "DateTime",
+                nullable: false,
+                defaultValue: new DateTime(2021, 6, 29, 14, 44, 44, 335, DateTimeKind.Local).AddTicks(8361),
+                oldClrType: typeof(DateTime),
+                oldType: "DateTime",
+                oldDefaultValue: new DateTime(2021, 6, 29, 14, 46, 7, 366, DateTimeKind.Local).AddTicks(9341));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_user-table",
+                table: "user-table",
+                column: "Id");
+        }
+    }
+}
