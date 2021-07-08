9ae8072c9fa53c0c3e25cca09e4898903f922a7d
htloc <lochatan19@gmail.com>
2021-07-08 17:32:44 +0700

    class handle insert database

---
+++ Infrastructure/Kafka/Consumer/HandleConsumer.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Models;
+using Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UserProtoBufService;
+
+namespace Infrastructure.Kafka.Consumer
+{
+    public class HandleConsumer
+    {
+        private IUserRepository _userRepository;
+
+        public HandleConsumer(IUserRepository userRepository)
+        {
+            this._userRepository = userRepository;
+        }
+        public void Handle(UserProtoReq req)
+        {
+//            if (req != null)
+            {
+                UserModel model = new UserModel
+                {
+                    Email = req.Email,
+                    Fullname = req.Fullname,
+                    //Id = req.UserId
+                };
+                this._userRepository.Insert(model);
+                this._userRepository.SaveChange();
+            }
+        }
+    }
+}
