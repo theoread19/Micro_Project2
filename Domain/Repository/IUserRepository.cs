79553c3dcb4d07ec1948e6b975069bf9e9f48ea5
htloc <lochatan19@gmail.com>
2021-07-08 17:31:05 +0700

    create user repo interface

---
+++ Domain/Repository/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Repository
+{
+    public interface IUserRepository
+    {
+        /*        ;
+                public MessageModel GetById(long id);
+                public List<MessageModel> GetBySenderId(long id);
+                public void Update(MessageModel model);
+                public void Delete(long id);
+                public void SaveChange();*/
+        public List<UserModel> GetAll();
+        public void Insert(UserModel model);
+        public void SaveChange();
+    }
+}
